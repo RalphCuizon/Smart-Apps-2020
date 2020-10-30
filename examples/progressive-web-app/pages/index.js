2b996d4545c7f98849ff9ba806309ca07ac2cd06
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2020-10-30 12:47:22 +0100

    update

---
+++ examples/progressive-web-app/pages/index.js
@@ -0,0 +1,113 @@
+import styles from '../styles/Home.module.css'
+import { useUser } from '../utils/auth/useUser'
+import useSWR from 'swr'
+import Link from 'next/link'
+
+const fetcher = (url, token) =>
+  fetch(url, {
+    method: 'GET',
+    headers: new Headers({ 'Content-Type': 'application/json', token }),
+    credentials: 'same-origin',
+  }).then((res) => res.json())
+
+const Index = () => {
+  const { user, logout } = useUser()
+  const { data, error } = useSWR(
+    user ? ['/api/getFood', user.token] : null,
+    fetcher
+  )
+  if (!user) {
+    return (
+      <>
+        <p>Hi there!</p>
+        <p>
+          You are not signed in.{' '}
+          <Link href={'/auth'}>
+            <a>Sign in</a>
+          </Link>
+        </p>
+        <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+      </>
+      
+    )
+  }
+
+  return (
+    <div>
+      <div>
+        <p>You're signed in. Email: {user.email}</p>
+        <p
+          style={{
+            display: 'inline-block',
+            color: 'blue',
+            textDecoration: 'underline',
+            cursor: 'pointer',
+          }}
+          onClick={() => logout()}
+        >
+          Log out
+        </p>
+      </div>
+      <div>
+        <Link href={'/releases'}>
+          <a>Releases</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Index
+
--- logboek.md
+++ logboek.md
@@ -4,4 +4,6 @@
 | 02/10/2020   | 5 Uren          | Individuele gesprek, Chapter 1 en Chapter 2 serviceworkies.com | 
 | 09/10/2020   | 4 Uren          | Idee/Informatie op Word Schrijven, Individuele gesprek + verder werken op serviceworkies.com |
 | 16/10/2020   | 3 Uren          | Balsamiq Cloud : Mockups Web en Mobile |
-| 23/10/2020   | 3 Uren          | Create a react pwa app |
+| 23/10/2020   | 3 Uren          | Create a react pwa app https://www.youtube.com/watch?v=IaJqMcOMuDM&ab_channel=JavaScriptMastery|
+| 27/10/2020   | 7 Uren          | Create a react pwa app, Create a Next.js App https://nextjs.org/learn/basics/create-nextjs-app, Firebase Login React https://github.com/vercel/next.js/tree/canary/examples/with-firebase-authentication |
+| 30/10/2020   | 3 Uren          | Next.js: auth + pwa, volgende keer niet werken met next.js |
